fd3939a4790e7a56274b2958ff7e26a698c35902
Daniel <danikzero@hotmail.com>
2025-01-15 15:31:09 -0300

    correções de bugs na captura de dados.

--- content.js
+++ content.js
@@ -19,7 +19,9 @@
             const content = celula.innerHTML
               .replace(/<[^>]+>/g, "") // Remove tags HTML
               .trim()
-              .replace(boldText, "").replace("\n", " ").trim(); // Remove o texto em negrito
+              .replace(boldText, "")
+              .replace("\n", " ")
+              .trim(); // Remove o texto em negrito
 
             if (boldText) {
               objetoAlvo[boldText.replace(":", "").trim()] =
@@ -43,44 +45,88 @@
         jsonData["cabecalho2"] = cabecalho2;
       }
 
+
+      let indice = 3;
       // Terceira tabela (Imagens e Orçamento)
       if (tabelas.length > 2) {
         const cabecalho3 = {};
         const celulas = tabelas[2].querySelectorAll("td");
-        cabecalho3["logo_esquerda"] = celulas[0]?.querySelector("img")?.src || "Logo esquerda não encontrada";
-        cabecalho3["titulo"] = celulas[1]?.innerText?.trim() || "Título não encontrado";
 
-        const textoLimpo =  cabecalho3["titulo"].replace(/\n/g, '');
-        const indiceNumero = textoLimpo.indexOf('Nº');
-        if (indiceNumero !== -1) {
-          // Extrai o número logo após 'Nº'
-          const numeroPedido = textoLimpo.slice(indiceNumero + 2).trim();
-          cabecalho3["numero_pedido"] = numeroPedido.trim() * 1 || "Título não encontrado";
-      } 
+        
 
-        cabecalho3["logo_direita"] = celulas[2]?.querySelector("img")?.src || "Logo direita não encontrada";
-        jsonData["cabecalho3"] = cabecalho3;
+        if (celulas[0]?.querySelector("img") !== null) {
+          cabecalho3["logo_esquerda"] =
+            celulas[0]?.querySelector("img")?.src ||
+            "Logo esquerda não encontrada";
+          cabecalho3["titulo"] =
+            celulas[1]?.innerText?.trim() || "Título não encontrado";
+
+          const textoLimpo = cabecalho3["titulo"].replace(/\n/g, "");
+          const indiceNumero = textoLimpo.indexOf("Nº");
+          if (indiceNumero !== -1) {
+            // Extrai o número logo após 'Nº'
+            const numeroPedido = textoLimpo.slice(indiceNumero + 2).trim();
+            cabecalho3["numero_pedido"] =
+              numeroPedido.trim() * 1 || "Título não encontrado";
+          }
+
+          cabecalho3["logo_direita"] =
+            celulas[2]?.querySelector("img")?.src ||
+            "Logo direita não encontrada";
+          jsonData["cabecalho3"] = cabecalho3;
+
+          indice = 3;
+        }else{
+          const cabecalho3 = {};
+          processarTabela(tabelas[2], cabecalho3);
+
+
+          const cabecalho4 = {};
+          const celulas = tabelas[3].querySelectorAll("td");
+          cabecalho4["logo_esquerda"] =
+            celulas[0]?.querySelector("img")?.src ||
+            "Logo esquerda não encontrada";
+          cabecalho4["titulo"] =
+            celulas[1]?.innerText?.trim() || "Título não encontrado";
+
+          const textoLimpo = cabecalho4["titulo"].replace(/\n/g, "");
+          const indiceNumero = textoLimpo.indexOf("Nº");
+          if (indiceNumero !== -1) {
+            // Extrai o número logo após 'Nº'
+            const numeroPedido = textoLimpo.slice(indiceNumero + 2).trim();
+            cabecalho4["numero_pedido"] =
+              numeroPedido.trim() * 1 || "Título não encontrado";
+          }
+          cabecalho4["logo_direita"] =
+            celulas[2]?.querySelector("img")?.src ||
+            "Logo direita não encontrada";
+
+          jsonData["cabecalho3"] = {...cabecalho3, ...cabecalho4};
+          indice = 4;
+        }
       }
 
       // Quarta tabela (Representada)
-      if (tabelas.length > 3) {
+      if (tabelas.length > indice) {
         const cabecalho4 = {};
-        processarTabela(tabelas[3], cabecalho4);
+        processarTabela(tabelas[indice], cabecalho4);
         jsonData["cabecalho4"] = cabecalho4;
+        indice ++;
       }
 
       // Quinta tabela (Informações do Cliente)
-      if (tabelas.length > 4) {
+      if (tabelas.length > indice) {
         const cabecalho5 = {};
-        processarTabela(tabelas[4], cabecalho5);
+        processarTabela(tabelas[indice], cabecalho5);
         jsonData["cabecalho5"] = cabecalho5;
+        indice ++;
       }
 
       //console.log("JSON gerado:", jsonData);
 
       // Processa a tabela de índice 5
-      if (tabelas.length > 5) {
-        const tabela = tabelas[5];
+      if (tabelas.length > indice) {
+        const tabela = tabelas[indice];
         const linhas = tabela.querySelectorAll("tbody > tr");
 
         if (linhas.length > 1) {
@@ -95,28 +141,73 @@
                 "Detalhe não encontrado";
 
               const objeto = {
-                "id": parseInt(celulas[0]?.innerText?.trim(), 10) || null,
-                "codigo": celulas[1]?.innerText?.trim() || "Código não encontrado",
-                "produto": produtoDetalhe,
-                "quantidade": parseInt(celulas[4]?.innerText?.trim(), 10) || 0,
-                "preco_tabela": celulas[5]?.innerText?.trim().replace('R$ ', '').replace('.','').replace(',', '.') * 1  || "Preço não encontrado",
-                "desconto": celulas[6]?.innerText?.trim() || "Desconto não encontrado",
-                "preco_liquido": celulas[7]?.innerText?.trim().replace('R$ ', '').replace('.','').replace(',', '.') * 1  || "Preço Líquido não encontrado",
-                "subtotal": celulas[8]?.innerText?.trim().replace('R$ ', '').replace('.','').replace(',', '.') * 1 || "Subtotal não encontrado",
+                id: parseInt(celulas[0]?.innerText?.trim(), 10) || null,
+                codigo: celulas[1]?.innerText?.trim() || null,
+                produto: produtoDetalhe,
+                quantidade: parseInt(celulas[4]?.innerText?.trim(), 10) || 0,
+                preco_tabela:
+                  celulas[5]?.innerText
+                    ?.trim()
+                    .replace("R$ ", "")
+                    .replace(".", "")
+                    .replace(",", ".") * 1 || null,
+                desconto:
+                  (celulas[6]?.innerText?.trim().replace("%", "") * 1) / 100 ||
+                  null,
+                preco_liquido:
+                  celulas[7]?.innerText
+                    ?.trim()
+                    .replace("R$ ", "")
+                    .replace(".", "")
+                    .replace(",", ".") * 1 || null,
+                subtotal:
+                  celulas[8]?.innerText
+                    ?.trim()
+                    .replace("R$ ", "")
+                    .replace(".", "")
+                    .replace(",", ".") * 1 || null,
               };
 
               jsonData["item"].push(objeto);
-            } 
-            /*else {
-              console.warn(`Linha ${i + 1} com células insuficientes.`);
-            }*/
+            }
           }
 
-          console.log("JSON gerado:", { cabecalho: 
-            {...jsonData.cabecalho1, ...jsonData.cabecalho2, ...jsonData.cabecalho3, ...jsonData.cabecalho4, ...jsonData.cabecalho5}, 
-            detalhes: jsonData.item, });
-        } 
-        else {
+          const payload = {
+            cabecalho: {
+              ...jsonData.cabecalho1,
+              ...jsonData.cabecalho2,
+              ...jsonData.cabecalho3,
+              ...jsonData.cabecalho4,
+              ...jsonData.cabecalho5,
+            },
+            detalhes: jsonData.item,
+          };
+
+          // Fetch para enviar o JSON à API
+          fetch("http://localhost:8083/api/v1/pedidos", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(`Erro: ${response.statusText}`);
+              }
+              return response.json();
+            })
+            .then((data) => {
+              //console.log("Resposta da API:", data['status']);
+              alert("Resposta da API: " + data["status"]);
+            })
+            .catch((error) => {
+              alert(
+                `Erro ao enviar os dados:\n${error.message}\nCertifique-se de que este pedido já tenha sido importado!`
+              );
+              console.error("Detalhes do erro:", error); // Para mais informações no console
+            });
+        } else {
           console.warn("Nenhuma linha encontrada na tabela além do cabeçalho.");
         }
       } else {
@@ -126,6 +217,8 @@
       console.warn("Nenhuma tabela encontrada.");
     }
   } else {
-    console.error('Div com ID "conteudo_imprimir" não encontrada!');
+    alert(
+      "Certifique-se de acessar a página de impressão do pedido ou de tê-la aberto ao menos uma vez."
+    );
   }
 })();
